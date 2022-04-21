111875c3eb44ee18f07a7baadbf4ae5d13042d72
seungchae1 <s2125@e-mirim.hs.kr>
2022-04-21 20:05:00 +0900

    MainActivity.java로 화면 만들기

--- app/src/main/java/kr/hs/emirim/s2125/mirim_project_0421_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2125/mirim_project_0421_1/MainActivity.java
@@ -2,13 +2,21 @@ package kr.hs.emirim.s2125.mirim_project_0421_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        LinearLayout rootLayout = new LinearLayout(this); //this => 현재 activity
+        rootLayout.setOrientation(LinearLayout.VERTICAL);
+        rootLayout.setBackgroundColor(Color.rgb(0,255,255));
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT);
+        setContentView(rootLayout, params);
     }
 }
